cfc5b444e18688c2cc08049afe1c4dd0ad1f151a
Petri Paavola <petri@petripaavola.fi>
2022-11-03 15:01:15 +0200

    Update Intune_Linux_Custom_Compliance_script.sh
    Added pwsh existence check, Powershell version check, MSEdge check

--- Linux/Intune_Linux_Custom_Compliance_script.sh
+++ Linux/Intune_Linux_Custom_Compliance_script.sh
@@ -5,7 +5,10 @@
 # which is included in this script
 #
 # Compliance checks:
+#	Check Powershell is installed
+#	Check Powershell version
 #	Reboot Required check (file should not exist /var/run/reboot-required)
+#   	Check MS Edge is installed (file should exist /opt/microsoft/msedge/msedge
 #
 #
 # Script creates 2 log files for debugging
@@ -27,15 +30,15 @@
 # https://github.com/petripaavola/Intune/tree/master/Linux
 
 
-Version=1.60
+Version=2.25
 BashLogFilePath=/tmp/IntuneCustomComplianceScript_Bash.log
 
 # Create log for debugging
 echo "Intune Linux Custom Compliance bash script version $Version" > $BashLogFilePath
-echo "Runtime: $(date +%Y-%m-%d_%H:%M:%S)" >> $BashLogFilePath
+echo "Runtime: $(/bin/date +%Y-%m-%d_%H:%M:%S)" >> $BashLogFilePath
 echo "Running script: $0" >> $BashLogFilePath
-echo "Running as user: $( whoami; )" >> $BashLogFilePath
-echo "The present working directory is $( pwd; )" >> $BashLogFilePath
+echo "Running as user: $( /bin/whoami; )" >> $BashLogFilePath
+echo "Working directory: $( /bin/pwd; )" >> $BashLogFilePath
 
 # Create Powershell commands string
 PowerShellCommandsString='
@@ -67,10 +70,32 @@ PowerShellCommandsString='
 		$RebootRequired = $False
 	}
 	$logData += "RebootRequired=$RebootRequired"
-	$logData += "###################################################"
 
+	# Check MSEdge is installed
+	$logData += "###################################################"
+	$logData += "Run Microsoft Edge is installed check"
+	$logData += "file should exist /opt/microsoft/msedge/msedge"
+	if(Test-Path /opt/microsoft/msedge/msedge) {
+		$MSEdgeInstalled = $True
+	} else {
+		$MSEdgeInstalled = $False
+	}
+	$logData += "MSEdgeInstalled=$MSEdgeInstalled"
+
+	# Check Powershell version
+	$logData += "###################################################"
+	$logData += "Run Powershell version check"
+	[String]$PSVersion = $PSVersionTable | Select-Object -ExpandProperty PSVersion
+	$logData += "PSVersion=$PSVersion"
+
+	$logData += "###################################################"
 	# Create hash table which we convert to compressed JSON
-	$hash = @{ RebootRequired = $RebootRequired }
+	$hash = @{
+		RebootRequired = $RebootRequired
+		MSEdgeInstalled = $MSEdgeInstalled
+		PSVersion = $PSVersion
+		PowershellInstalled = $True
+	}
 	$returnJson = $hash | ConvertTo-Json -Compress
 
 	$logData += "Custom Compliance returnJson = $returnJson"
@@ -85,9 +110,25 @@ echo "####################################################" >> $BashLogFilePath
 echo "Running Powershell script: $PowerShellCommandsString" >> $BashLogFilePath
 echo "####################################################" >> $BashLogFilePath
 
-# Run Powershell Commands
-pwsh -Command "& { ${PowerShellCommandsString} }"
-ExitCode=$?
+# Check that pwsh exist
+PWSHPATH=/opt/microsoft/powershell/7/pwsh
+if [ -f "$PWSHPATH" ]; then
+	# Powershell found
+	# Run Powershell Commands
+	
+	echo "Run Powershell script part" >> $BashLogFilePath
+	$PWSHPATH -Command "& { ${PowerShellCommandsString} }"
+	ExitCode=$?
+	echo "Powershell script run exitcode: $ExitCode" >> $BashLogFilePath
+	echo "Bash script end" >> $BashLogFilePath	
+else
+	# pwsh not found
+	# return hardcoded json which will make device Not Compliant
 
-echo "Powershell script run exitcode: $ExitCode" >> $BashLogFilePath
-echo "Bash script end" >> $BashLogFilePath
+	echo "pwsh not found from path: $PWSHPATH" >> $BashLogFilePath
+	echo "Compliance Check will fail" >> $BashLogFilePath
+	JSON='{"RebootRequired":false,"MSEdgeInstalled":true,"PSVersion":"7.2.7","PowershellInstalled":false}'
+	echo "Return json: $JSON" >> $BashLogFilePath
+	echo "Bash script end" >> $BashLogFilePath
+	echo "$JSON"
+fi
